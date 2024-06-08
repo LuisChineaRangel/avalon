f9d32297ae83e02573734677d3790eccfa198209
iluzioDev <luischinearangel@gmail.com>
2024-06-08 01:45:34 +0100

    Updating

--- server/src/schemas/comment.schema.ts
+++ server/src/schemas/comment.schema.ts
@@ -2,7 +2,7 @@ import {Schema, model} from 'mongoose';
 import {Comment} from '@shared/interfaces/comment.interface';
 
 const CommentSchema = new Schema<Comment>({
-    author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    author: { type: String, required: true },
     post: { type: Schema.Types.ObjectId, ref: 'Post', required: true },
     content: { type: String, required: true },
     created_at: { type: Date, default: Date.now },
