6b93dde2d7d912e6f91fd153c67a1cdbde1def49
iluzioDev <luischinearangel@gmail.com>
2024-06-09 22:28:05 +0100

    Updating

--- server/src/routers/user.router.ts
+++ server/src/routers/user.router.ts
@@ -9,7 +9,17 @@ import Post from "@schemas/post.schema";
 
 export const userRouter = express.Router();
 
-const upload = multer({ dest: 'uploads/' });
+const storage = multer.diskStorage({
+    destination: function (_, __, cb) {
+        cb(null, "uploads/");
+    },
+    filename: function (_, file, cb) {
+        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        cb(null, file.fieldname + "-" + uniqueSuffix + ".jpg");
+    }
+});
+
+const upload = multer({ storage: storage });
 
 userRouter.use(bodyParser.json());
 
