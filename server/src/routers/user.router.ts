5d5a840c1af2364bb6010a91b73f375824f5de94
iluzioDev <luischinearangel@gmail.com>
2024-06-09 01:05:16 +0100

    Updating

--- server/src/routers/user.router.ts
+++ server/src/routers/user.router.ts
@@ -79,13 +79,12 @@ userRouter.patch("/users/:id", auth, async (req, res) => {
 
 userRouter.get("/users/username/:username", auth, async (req, res) => {
     try {
-        await User.findOne({ username: req.params.username }).then((user) => {
+        await User.findOne({ username: req.params.username }).then(async (user) => {
             if (!user)
                 return res.status(404).send("User not found");
             let user_posts: any;
-            Post.find({ author: user.username }).then((posts) => {
+            await Post.find({ author: user.username }).then((posts) => {
                 user_posts = posts;
-                return res.status(200).send(user);
             }).catch((err) => {
                 console.log(err);
                 return res.status(500).send("Internal server error");
