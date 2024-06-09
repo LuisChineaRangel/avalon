b9032be5f86ca4d0fa81ac0cc134127775edc200
iluzioDev <luischinearangel@gmail.com>
2024-06-09 19:55:33 +0100

    Update

--- server/src/routers/user.router.ts
+++ server/src/routers/user.router.ts
@@ -105,6 +105,7 @@ userRouter.get("/users/username/:username", auth, async (req, res) => {
 
 userRouter.post("/users/upload", auth, upload.single('file'), async (req, res) => {
     try {
+        console.log(req.file);
         if (!req.file)
             return res.status(400).send("No file uploaded");
 
